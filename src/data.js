4a1d9f8f15956b6d2e8276071c7ceb25a3101320
sofiya-mathew <sofiyamathew99@gmail.com>
2022-12-08 20:36:54 +0530

    imageChange

--- src/data.js
+++ src/data.js
@@ -3,83 +3,83 @@ let shopData = [{
     name: 'Nike shoes',
     price: 2400,
     description: ' with comfy Air Max cushioning.',
-    Image: './images/nike'
+    Image: './images/nike.jpeg'
 },
 {
     id: 'hghj',
     name: 'Wildcraft',
     price: 1200,
     description: 'Unisex Black Lunar Laptop Backpack',
-    Image: './images/backpack'
+    Image: './images/backpack.jpg'
 },
 {
     id: 'hjgj',
     name: 'Shirt',
     price: 700,
     description: 'Comfortable to wear',
-    Image: './images/img1'
+    Image: './images/img1.jpg'
 },
 {
     id: 'hjgjvtbt',
     name: 'Buds 2',
     price: 999,
     description: 'Active Noise Cancellation. ',
-    Image: './images/airpod'
+    Image: './images/airpod.jpg'
 },
 {
     id: 'hjgjhgbdc',
     name: 'Jacket',
     price: 699,
     description: 'Comfortable to wear',
-    Image: './images/jacket'
+    Image: './images/jacket.jpg'
 },
 {
     id: 'hjgjdddd',
     name: 'Realme 5 pro',
     price: 15999,
     description: '48 pixel high quality Camera',
-    Image: './images/mobile'
+    Image: './images/mobile.jpg'
 },
 {
     id: 'hjgjdddddc',
     name: 'Samsung Earphone',
     price: 450,
     description: 'Powered with high bass',
-    Image: './images/earphone'
+    Image: './images/earphone.jpg'
 },
 {
     id: 'hjgjcw',
     name: 'Puma shoes',
     price: 3500,
     description: 'Comfortable to wear',
-    Image: './images/shoes'
+    Image: './images/shoes.jpg'
 },
 {
     id: 'hjgjdcec',
     name: 'MacBook Air',
     price: 100000,
     description: 'supercharged by M1 and M2 chips',
-    Image: './images/laptop'
+    Image: './images/laptop.jpg'
 },
 {
     id: 'hjgjxw',
     name: 'TShirt',
     price: 500,
     description: 'Comfortable to wear',
-    Image: './images/shirt'
+    Image: './images/shirt.jpg'
 },
 {
     id: 'hjgjfref',
     name: 'Watch',
     price: 800,
     description: 'Unisex watch',
-    Image: './images/watch'
+    Image: './images/watch.jpg'
 },
 {
     id: 'hjgjdef',
     name: 'Tshit',
     price: 689,
     description: 'Comfortable to wear',
-    Image: './images/tshirtgrls'
+    Image: './images/tshirtgrls.jpg'
 },
 ]
